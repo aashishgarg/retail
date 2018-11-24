4cdbcf3926c0c5932922860c6570056b0143b5c7
Ashish Garg <ashish2.kumar@trantorinc.com>
2018-11-24 12:59:10 +0530

    images of varians added

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -2,4 +2,5 @@ class Variant < ApplicationRecord
   # --- Associations --- #
   belongs_to :product, inverse_of: :variants
   has_and_belongs_to_many :attribute_choices
+  has_many_attached :images
 end
